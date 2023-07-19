2c56ed2840234325564ae596db86e2d799dcf483
Jhumel Bonganciso <theodinprojectjhumel@gmail.com>
2023-07-19 18:19:19 +0800

    done with the exercises

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log(2+2);
+
+console.log(1+2+3+4+5+6);
+
+console.log(((4+6+9)/77).toFixed(5))
+
+console.log((19/77).toFixed(5))
+
+let a = 10;
+
+console.log(10)
+
+let b = 7 * a;
+
+console.log(b)
+
+const max = 57;
+
+let actual = max -13
+
+let percentage = actual/max;
+
+console.log(percentage.toFixed(4))
